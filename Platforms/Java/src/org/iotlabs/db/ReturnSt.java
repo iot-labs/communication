a8332cf6aecc770b225ae73a8d5d490d94871d93
JongKwang Kim <kim@jongkwang.com>
2016-04-19 23:41:04 +0900

    JDBC 모듈 개발 중 커밋

---
+++ Platforms/Java/src/org/iotlabs/db/ReturnSt.java
@@ -0,0 +1,14 @@
+package org.iotlabs.db;
+
+import java.util.List;
+
+public class ReturnSt {
+	
+	public boolean session = true;	// default = true
+	public String errorMsg = null;
+	public String exceptionMsg = null;
+	public Object result = null;
+	
+	public String timestamp = null;
+	
+}
