9bd5b6ffb0f4424f1033af4702990aad25b0fa01
JongKwang Kim <kim@jongkwang.com>
2016-04-20 21:32:28 +0900

    Mqtt PahoReceiver : MQTT Broker 의 메세지 수신 클래스 개발

---
+++ Platforms/Java/src/org/iotlabs/communication/mqtt/PahoReceiver.java
@@ -0,0 +1,14 @@
+package org.iotlabs.communication.mqtt;
+
+/**
+ * MQTT Broker 의 메세지를 수신한다.
+ * @author JongKwang
+ *
+ */
+public class PahoReceiver {
+	
+	
+	public static void main(String[] args) {
+	}
+
+}
