cdc8ca1c7a07453772933d80714fca8f31ec24e4
JongKwang Kim <kim@jongkwang.com>
2016-05-11 21:36:40 +0900

    Util Class : ConsoleCmd 개발 중 커밋

---
+++ Platforms/Java/src/org/iotlabs/util/ConsoleCmd.java
@@ -0,0 +1,18 @@
+package org.iotlabs.util;
+
+
+/**
+ * Java 에서 Linux Console 명령을 호출 한다.
+ * @author JongKwang
+ *
+ */
+public class ConsoleCmd {
+	
+	public ConsoleCmd() {
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("- ConsoleCmd 시작 #####");
+	}
+}
