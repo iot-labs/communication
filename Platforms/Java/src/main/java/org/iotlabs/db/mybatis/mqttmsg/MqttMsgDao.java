5ab4fcf40777056d64d59039f860fd7b71cfe4ee
january89 <eightynine01@naver.com>
2016-09-26 22:10:54 +0900

    Gradle javaspark 의존성 추가
    MqttMsgMapper Path 수정
    
    Mybatis Configuration 수정
    Mybatis MqttMsgDao 수정

--- Platforms/Java/src/main/java/org/iotlabs/db/mybatis/mqttmsg/MqttMsgDao.java
+++ Platforms/Java/src/main/java/org/iotlabs/db/mybatis/mqttmsg/MqttMsgDao.java
@@ -1,6 +1,5 @@
 package org.iotlabs.db.mybatis.mqttmsg;
 
-import com.google.gson.Gson;
 import org.apache.ibatis.session.SqlSession;
 import org.iotlabs.db.mybatis.MyBatisManager;
 import org.iotlabs.models.mqtt.MqttMsg;
@@ -9,38 +8,33 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MqttMsgDao {
-  private Gson gson = new Gson();
   private SqlSession session = MyBatisManager.getSqlMapper().openSession();
-  private int result;
 
   private HashMap<String,MqttMsg> mqttMsgSelectOne(String query,MqttMsg msg){
-    return session.selectOne(query,msg);
+    HashMap<String,MqttMsg> result = session.selectOne(query,msg);
+    session.close();
+    return result;
   }
   private List<MqttMsg> mqttMsgSelectList(String query, MqttMsg msg){
-    return session.selectList(query,msg);
+    List<MqttMsg> result = session.selectList(query,msg);
+    session.close();
+    return result;
   }
   private int mqttMsgInsert(String query,MqttMsg map){
-    this.result = session.insert(query,map);
-    if(result == 1)
-      session.commit();
-    else
-      session.rollback();
-    return result;
+    return wrapSession(session.insert(query,map));
   }
   private int mqttMsgUpdate(String query,MqttMsg map){
-    this.result = session.update(query,map);
-    if(result == 1)
-      session.commit();
-    else
-      session.rollback();
-    return result;
+    return wrapSession(session.update(query,map));
   }
   private int mqttMsgDelete(String query,MqttMsg map){
-    this.result = session.delete(query,map);
+    return wrapSession(session.delete(query,map));
+  }
+  private int wrapSession(int result){
     if(result == 1)
       session.commit();
     else
       session.rollback();
+    session.close();
     return result;
   }
 }
