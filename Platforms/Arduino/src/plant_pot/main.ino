6f45fdc4fde8c71467463f248117588fd857b59a
JongsoonHwang <jongsoonhwang80@gmail.com>
2016-09-28 05:49:21 +0900

    serial 통신 통한 로그 출력 확인

--- Platforms/Arduino/src/plant_pot/main.ino
+++ Platforms/Arduino/src/plant_pot/main.ino
@@ -1,5 +1,14 @@
+#include <Debug.h>
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3, 2); // RX, TX
 void setup() {
   // put your setup code here, to run once:
+	char c;
+
+	mySerial.begin(9600);
+	Serial.begin(9600);
+    Serial.println("Serial communication test");
 
 }
 
