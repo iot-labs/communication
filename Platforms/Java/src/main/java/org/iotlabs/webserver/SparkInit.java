c11842c530c26f6be04825a6801d3d111aed44a2
toori67 <toori67@gmail.com>
2016-09-27 15:27:28 +0900

    add test code

--- Platforms/Java/src/main/java/org/iotlabs/webserver/SparkInit.java
+++ Platforms/Java/src/main/java/org/iotlabs/webserver/SparkInit.java
@@ -5,6 +5,7 @@ import org.iotlabs.util.Configuration;
 
 import java.io.IOException;
 
+import static spark.Spark.ipAddress;
 import static spark.Spark.port;
 import static spark.Spark.threadPool;
 
@@ -20,9 +21,10 @@ class SparkInit {
             logger.info("Configuration not found. Run server with default settings.");
             return;
         }
-        port(configuration.getInteger("port"));
+        ipAddress(configuration.getString("host", "0.0.0.0"));
+        port(configuration.getInteger("port", 4567));
         int minThread = configuration.getInteger("min_thread", 2);
-        int maxThread = configuration.getInteger("max_thread", 8);
+        int maxThread = configuration.getInteger("max_thread", 9);
         int timeoutInMillis = configuration.getInteger("timeout", 30000);
         threadPool(maxThread, minThread, timeoutInMillis);
     }
