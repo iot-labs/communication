b51040f75db381ae162718d52f5409ae43403aa6
JongsoonHwang <jongsoonhwang80@gmail.com>
2016-09-28 06:00:35 +0900

    dth11 온습도 센서 값 읽기

--- Platforms/Arduino/src/plant_pot/main.ino
+++ Platforms/Arduino/src/plant_pot/main.ino
@@ -1,6 +1,9 @@
 #include <Debug.h>
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "DHT11.h"
+DHT11 dht11(6);
 SoftwareSerial mySerial(3, 2); // RX, TX
 void setup() {
   // put your setup code here, to run once:
@@ -15,4 +18,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+Serial.print("temperature : "); Serial.print(temp);
+int error;
+float temp, humidity;
+            if(error = dht11.read(humidity, temp) == 0) {
+              Serial.print("humidity: ");  Serial.print(humidity);
+
+}
 }
