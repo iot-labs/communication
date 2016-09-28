e8d086cac99594883face5ce44d379ca153943c4
january89 <eightynine01@naver.com>
2016-09-28 10:31:52 +0900

    버튼 페이지 생성 작업 중간저장

--- Platforms/Java/src/main/java/org/iotlabs/communication/mqtt/MqttController.java
+++ Platforms/Java/src/main/java/org/iotlabs/communication/mqtt/MqttController.java
@@ -1,5 +1,6 @@
 package org.iotlabs.communication.mqtt;
 
+import org.iotlabs.models.mqtt.MqttMsg;
 import spark.ModelAndView;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
@@ -11,7 +12,10 @@ import static spark.Spark.*;
 public class MqttController{
   public static void main(String[] args) {
     Map map = new HashMap<>();
+    MqttMsg msg = new MqttMsg();
+    ThymeleafTemplateEngine engine = new ThymeleafTemplateEngine();
     map.put("hello","Hello World");
-    get("/hello",(request,response)->new ModelAndView(map,"hello"),new ThymeleafTemplateEngine());
+    get("/hello",(request,response)->new ModelAndView(map,"hello"),engine);
+    get("/LedSwitcher",(request,response)->new ModelAndView(msg,"LedSwitcher"),engine);
   }
 }
