9674d8b59ae858bbd85be139158940e088776588
sejin.jang.0128 <sejin.jang.0128@gmail.com>
2016-09-30 22:41:11 +0900

    Add Xiaomi YeeLight Control project

---
+++ Projects/LED_YeeLightControl/src/yeelightblue/Tutorial_Test.java
@@ -0,0 +1,11 @@
+package yeelightblue;
+
+public class Tutorial_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("BLE 통신 완료");
+	}
+
+}
