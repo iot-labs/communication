72253127e661141e0db7a84ff3bccd9000a4893b
january89 <eightynine01@naver.com>
2016-09-30 17:54:08 +0900

    임시저장

--- Platforms/Java/src/main/java/org/iotlabs/projects/led_tutorial/webclient/MqttController.java
+++ Platforms/Java/src/main/java/org/iotlabs/projects/led_tutorial/webclient/MqttController.java
@@ -1,7 +1,8 @@
 package org.iotlabs.projects.led_tutorial.webclient;
 
-import org.iotlabs.models.mqtt.MqttMsg;
+import org.apache.log4j.Logger;
 import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,10 +10,14 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class MqttController{
+  private static Logger log = Logger.getLogger(MqttController.class);
+
   public static void main(String[] args) {
+    log.debug("===================로그");
     Map map = new HashMap<>();
-    MqttMsg msg = new MqttMsg();
-    MustacheTemplateEngine engine = new MustacheTemplateEngine();
-    get("/LedSwitcher",(request,response)->new ModelAndView(msg,"LedSwitcher"),engine);
+    map.put("text","텍스트");
+    port(8080);
+    get("/",(req,res)->new ModelAndView(map,"main"),new ThymeleafTemplateEngine());
+    get("/led",(req,res)->new ModelAndView(map,"/ledswitcher"),new ThymeleafTemplateEngine());
   }
 }
