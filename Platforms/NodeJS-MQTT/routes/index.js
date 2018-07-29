a78eafef2811b1e45e07917737d0526f5b25b308
eisen <eisen1990@naver.com>
2018-07-29 13:11:19 +0900

    MariaDB(MySQL)을 위한 셋업
    MQTT 토픽 서비스, 계정 등을 위한 준비

--- Platforms/NodeJS-MQTT/routes/index.js
+++ Platforms/NodeJS-MQTT/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var setup = require('../setup');
+var db_config = setup.DB_CONFIG;
+
+var mysql = require('mysql');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+    var obj;
+
+    try {
+        var conn = mysql.createConnection(db_config);
+
+        var query = conn.query("select * from topic_table;", function (err, rows) {
+            conn.end();
+
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+
+            obj = {'obj': rows.length > 0 ? rows : 'No data' };
+            console.log(obj);
+        });
+    } catch {
+        obj = "query error";
+    }
+
+    res.render('index', obj);
 });
 
 module.exports = router;
--- Platforms/NodeJS-MQTT/setup.js
+++ Platforms/NodeJS-MQTT/setup.js
@@ -1,7 +1,21 @@
-module.exports = {
-    //MongoDB, MQTT Host 등 세팅 설정
+/**
+ * MariaDB, MQTT Host 등 세팅 설정
+ * @type {{MARIADB_LOCALHOST: string, MQTT_LOCALHOST: string}}
+ */
 
-    MONGODB_LOCALHOST : "localhost",
+module.exports = {
+
+    /**
+     *  MariaDB, MySQL 기준 세팅
+     *  MySQL 8.x 버전부터는 SHA-256 방식을 사용한다.
+     */
+    DB_CONFIG : {
+        host : 'localhost',               // DB 호스팅 URL
+        port : 3306,                      // DB 포트 번호
+        user : 'root',                    // DB 유저, 일반 계정일 경우 DB에서 privilege 설정을 해야 접근 가능
+        password : 'Your DB Password',    // DB 유저의 비밀번호
+        database : 'Your DB Name'         // DB 이름
+    },
 
     MQTT_LOCALHOST : "mqtt://localhost"
 }
